d57f53b54bc471279af21636e129bfad0c7e94bb
nav-ios <131914355+nav-ios@users.noreply.github.com>
2026-09-23 15:33:07 -0400

    Set up project: iOS 17 target, unit and UI test targets, shared `CI` scheme, Info.plist for camera and photo library

---
+++ RogersMarketPlace/App/RogersMarketPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RogersMarketPlaceApp.swift
+//  RogersMarketPlace
+//
+//  Created by Navdeep Rana on 23/09/26.
+//
+
+import SwiftUI
+
+@main
+struct RogersMarketPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Marketplace")
+        }
+    }
+}
